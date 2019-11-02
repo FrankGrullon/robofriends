47c7d528c04915d464665e22d96ac5f0e385d0ae
Frank Grullon <frankvhalen@gmail.com>
2019-11-02 16:00:48 -0500

    Updates

---
+++ precache-manifest.bcb3f58792cb9de399fff2bde61a15d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f365034a9bad7afe5bb756e9086b52",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "0c1bcef0dab6ce3c20b6",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e0b2591b85535a74b420",
+    "url": "/robofriends/static/css/main.09921500.chunk.css"
+  },
+  {
+    "revision": "0c1bcef0dab6ce3c20b6",
+    "url": "/robofriends/static/js/2.d75c0efc.chunk.js"
+  },
+  {
+    "revision": "e0b2591b85535a74b420",
+    "url": "/robofriends/static/js/main.30056d6f.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
